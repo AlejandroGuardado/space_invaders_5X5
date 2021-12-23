d9e584b02eb77b67e7d15a65785e2f797f50da38
CerosWare <alexgk89@hotmail.com>
2021-12-23 03:03:41 -0700

    Added LevelSpot

---
+++ Assets/Scripts/LevelSpot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpot{
+    public EnemyType EnemyType { get; set; }
+    public LevelSpotStatus Status { get; set; }
+    public Vector2 position;
+
+    public LevelSpot() {
+        EnemyType = EnemyType.None;
+        Status = LevelSpotStatus.Standby;
+    }
+}
+
+/// <summary>
+/// Standby = Enemy has not spawned yet
+/// Active = Spot needs updating
+/// Off = Enemy was killed; no longer needs updating
+/// </summary>
+public enum LevelSpotStatus {
+    Standby,
+    Active,
+    Off
+}
