a2dacbec6df9aa936a0f2a215be93e15823c9ee8
CerosWare <alexgk89@hotmail.com>
2021-12-23 03:04:05 -0700

    Updated Session Screen
    InitSession, LoadLevel routine (init), SetLevelIndex

--- Assets/Scripts/SessionScreen.cs
+++ Assets/Scripts/SessionScreen.cs
@@ -16,7 +16,9 @@ public class SessionScreen : GameScreen{
     public UnityEvent OnDefeat;
 
     private int levelIndex;
+    private LevelData levelData;
     private int score;
+    private List<LevelSpot> grid;
 
     private void Awake() {
         levelIndex = 0;
@@ -28,10 +30,7 @@ public class SessionScreen : GameScreen{
         TransitionManager.Instance.CutInImmediatly();
         yield return new WaitForSeconds(transitionData.sessionDelayTime);
         TransitionManager.Instance.CutOut(transitionData.sessionTransitionTime);
-
-        //Init Session
-        InitScore();
-
+        InitSession();
         yield return new WaitForSeconds(transitionData.sessionTransitionTime);
     }
 
@@ -47,6 +46,73 @@ public class SessionScreen : GameScreen{
         return 0f;
     }
 
+    public void SetLevelIndex(int levelIndex) {
+        //Level Index from button events converted to zero-based
+        this.levelIndex = levelIndex - 1;
+    }
+
+    private void InitSession() {
+        InitScore();
+        LoadLevel();
+    }
+
+    private void LoadLevel() {
+        if (levelIndex < 0 || levelIndex > sessionData.levels?.Length - 1) {
+            //Converted back to one-based
+            Debug.LogError($"Level {++levelIndex} was not found");
+            return;
+        }
+        levelData = sessionData.levels[levelIndex];
+
+        //How many spots?
+        int spots = 0;
+        for (int i = 0; i < levelData.lines.Length; i++) {
+            spots += levelData.lines[i].NumberSpots;
+        }
+        int repeat = levelData.repeat > 1 ? levelData.repeat : 1;
+        spots *= repeat;
+
+        //Generate grid
+        grid = new List<LevelSpot>(spots);
+        List<int> spotEnemyAssigment = new List<int>(spots);
+        for (int i = 0; i < spots; i++) {
+            grid.Add(new LevelSpot());
+            spotEnemyAssigment.Add(i);
+        }
+
+        //Assign enemies randomly, remaining spots are set to Green
+        AssignRandomEnemyToSpots(spotEnemyAssigment, EnemyType.Red, levelData.redEnemiesNeeded);
+        AssignRandomEnemyToSpots(spotEnemyAssigment, EnemyType.Blue, levelData.blueEnemiesNeeded);        
+        for (int i = 0; i < spotEnemyAssigment.Count; i++) {
+            grid[spotEnemyAssigment[i]].EnemyType = EnemyType.Green;
+        }
+
+        //Starting position for spots
+        //Per line, each "xposition" corresponds to a spot. 
+        //O(n3) may be improved by caching positions per line
+        float lineHeight = sessionData.gridStartPosition;
+        int currentSpotIndex = 0;
+        for (int rep = 0; rep < repeat; rep++) {
+            for (int lineIndex = 0; lineIndex < levelData.lines.Length; lineIndex++) {
+                LevelLine line = levelData.lines[lineIndex];
+                for (int positionIndex = 0; positionIndex < line.NumberSpots; positionIndex++) {
+                    grid[currentSpotIndex].position = new Vector2(line.xPositions[positionIndex], lineHeight);
+                    currentSpotIndex++;
+                }
+                lineHeight += sessionData.gridLineHeight;
+            }
+        }
+    }
+
+    private void AssignRandomEnemyToSpots(List<int> spotIndexes, EnemyType type, int needed) {
+        for (int i = 0; i < needed; i++) {
+            if (spotIndexes.Count == 0) break;
+            int randomIndex = Random.Range(0, spotIndexes.Count);
+            grid[spotIndexes[randomIndex]].EnemyType = type;
+            spotIndexes.RemoveAt(randomIndex);
+        }
+    }
+
     private void HideUI() {
         scoreCanvas.SetActive(false);
         powerupCanvas.SetActive(false);
@@ -54,6 +120,7 @@ public class SessionScreen : GameScreen{
     }
 
     private void InitScore() {
+        score = 0;
         scoreCanvas.SetActive(true);
         UpdateScoreText();
     }
