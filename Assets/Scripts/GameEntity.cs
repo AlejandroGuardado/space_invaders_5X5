d4ca534a4bea66a13edf3cb10e1a02882ede0f13
CerosWare <alexgk89@hotmail.com>
2021-12-23 17:10:27 -0700

    Updated GameEntity
    Setting Active flag, toggling GameObject

--- Assets/Scripts/GameEntity.cs
+++ Assets/Scripts/GameEntity.cs
@@ -7,11 +7,15 @@ public class GameEntity : MonoBehaviour{
     public bool Active { get; private set; }    
 
     public virtual void Activate() {
+        gameObject.SetActive(true);
         sprite.enabled = true;
+        Active = true;
     }
 
     public virtual void Deactivate() {
+        gameObject.SetActive(false);
         sprite.enabled = false;
+        Active = false;
     }
 
     public virtual void Spawn(Vector2 position) {
