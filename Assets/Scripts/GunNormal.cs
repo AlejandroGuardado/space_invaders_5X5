f4ed767416db4cb3a8caa39ce8d7f2aa7508a940
CerosWare <alexgk89@hotmail.com>
2021-12-26 17:52:38 -0700

    Updated GunNormal

--- Assets/Scripts/GunNormal.cs
+++ Assets/Scripts/GunNormal.cs
@@ -6,7 +6,7 @@ public class GunNormal : Gun {
     public override bool OnFire(Vector2 position) {
         Bullet bullet = pool.Spawn(position);
         if (bullet) {
-            bullet.Fire(Vector2.up, speed, color);
+            bullet.Fire(Bullet.DEFAULT_DIRECTION, speed, color);
             return true;
         }
         return false;
