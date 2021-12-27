cac02b5ef7599317741f318ad7bfbd08f20e378e
CerosWare <alexgk89@hotmail.com>
2021-12-26 17:52:20 -0700

    Updated Bullet
    Rotation

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : GameEntity{
+    public static readonly Vector2 DEFAULT_DIRECTION = Vector2.up;
+    public static readonly float DEFAULT_ROTATION;
+
     public SessionData sessionData;
     public BoxCollider2D box;
     public SpriteRenderer glow;
     public float speed;
     private Vector2 direction;
+
+    static Bullet() {
+        DEFAULT_ROTATION = Mathf.Atan2(DEFAULT_DIRECTION.y, DEFAULT_DIRECTION.x);
+    }
 
     void Update() {
         if (Active) {
@@ -27,11 +34,21 @@ public class Bullet : GameEntity{
     public override void Deactivate() {
         box.enabled = false;
         glow.enabled = false;
+        speed = 0f;
+        glow.color = Color.white;
+        transform.rotation = Quaternion.identity;
         base.Deactivate();
     }
 
     public void Fire(Vector2 direction, float speed, Color color) {
         this.direction = direction;
+
+        //Rotation
+        float directionRotation = Mathf.Atan2(direction.y, direction.x);
+        directionRotation -= DEFAULT_ROTATION;
+        directionRotation *= Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, directionRotation);
+
         this.speed = speed;
         glow.color = new Color(color.r, color.g, color.b, glow.color.a);
     }
